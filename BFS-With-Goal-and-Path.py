7cd11a4950c58eb22a13be0cf0f384e96f3ae54f
Kazi Golam Mahtab <143117451+Mahtab0204@users.noreply.github.com>
2026-04-04 10:27:45 +0600

    Add BFS function to find path to a goal node
    Implement BFS algorithm to find path to goal.

---
+++ BFS-With-Goal-and-Path.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+def bfsWithGoalAndPath(graph,root,goal):
+
+    queue = deque([[root]])
+    visited = []
+
+    while queue:
+        path = queue.popleft()
+        node = path[-1]
+
+        if node==goal:
+            return path
+
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                visited.append(neighbor)
+                queue.append(path+[neighbor])
+
+
+
+
+
+Graph={
+    'A': ['B','C'],
+    'B': ['A','D','E'],
+    'C': ['A','F'],
+    'D': ['B','H'],
+    'E': ['B','G'],
+    'F': ['C','G'],
+    'G': ['E','F','I'],
+    'H': ['D','I'],
+    'I': ['G','H'],
+}
+
+path =  bfsWithGoalAndPath(Graph,'A','I')
+
+if  path:
+    print(path)
+else:
+    print("Path Not Found")
